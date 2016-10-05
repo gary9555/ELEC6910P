b82a7fddf75d21a407727e0f628ea89b49a6640d
wenr <wenr@student.ethz.ch>
2016-10-06 02:06:33 +0800

    .

---
+++ proj1phase2/code/trajectory_generator.m
@@ -0,0 +1,12 @@
+function s_des = trajectory_generator(t, path, h)
+
+
+if nargin > 1 % pre-process can be done here (given waypoints). Pre-define the entire trajectory.
+
+else % output desired state along the trajectory here (given time).
+    
+end
+
+end
+
+
